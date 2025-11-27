d8fa5f03276898ae86262f8b673ab48aac526ca4
Şükrü KÜÇÜK <sukrutkrdg@gmail.com>
2025-11-27 14:33:10 +0300

    ilk commitk33

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Vite projelerinde env değişkenleri import.meta.env üzerinden okunur.
-// Değişkenlerin başında VITE_ olması şarttır.
+// Değişkenlerin başında VITE_ olooooooooooması şarttır.
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
